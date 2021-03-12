fcdc33d557e78984e653663dc5f90a13fd5e561d
Wagner Araújo <wagneraraujo15@gmail.com>
2021-03-12 16:41:10 -0300

    create path store/produtos

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,4 @@
 import { createStore } from "redux";
-import Produtos from "../services/data";
 
-function reducer(state = [Produtos][0], action) {
-  return state;
-}
-
+import reducer from "../store/produtos";
 export default createStore(reducer);
---
+++ src/store/produtos/index.js
@@ -0,0 +1,6 @@
+import Produtos from "../../services/data";
+
+function reducer(state = [Produtos][0], action) {
+  return state;
+}
+export default reducer;
